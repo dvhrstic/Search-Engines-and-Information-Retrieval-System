f1d353a6eecd6f8b3473770dbfa1c70cef23121a
hrstic <hrstic@kth.se>
2018-02-12 19:08:28 +0100

    First commit

---
+++ Labb1-new_version/ir/HashedIndex.java
@@ -0,0 +1,57 @@
+/*
+ *   This file is part of the computer assignment for the
+ *   Information Retrieval course at KTH.
+ *
+ *   Johan Boye, 2017
+ */
+
+
+package ir;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+
+/**
+ *   Implements an inverted index as a Hashtable from words to PostingsLists.
+ */
+public class HashedIndex implements Index {
+
+
+    /** The index as a hashtable. */
+    private HashMap<String,PostingsList> index = new HashMap<String,PostingsList>();
+
+
+    /**
+     *  Inserts this token in the hashtable.
+     */
+    public void insert( String token, int docID, int offset ) {
+	if (index.containsKey(token)){
+        index.get(token).addID(docID, offset);
+    }else{
+        PostingsList postingsList = new PostingsList();
+        postingsList.addID(docID, offset);
+        index.put(token, postingsList);
+        }
+    }
+
+    /**
+     *  Returns the postings for a specific term, or null
+     *  if the term is not in the index.
+     */
+    public PostingsList getPostings( String token ) {
+    if (!index.containsKey(token)){
+        System.out.println("There is no such a token! ");
+        return null;
+    }else{
+        return index.get(token);
+        }
+    }
+
+
+    /**
+     *  No need for cleanup in a HashedIndex.
+     */
+    public void cleanup() {
+    }
+}
